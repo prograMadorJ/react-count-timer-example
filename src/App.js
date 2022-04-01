b1119b6ac82f6f3477b252f2e33ba437babd1b19
prograMadorJ <jorge.fernando.brito@gmail.com>
2022-04-01 00:55:16 -0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CountTimer from './CountTimer';
 export default function App() {
   return (
     <>
-    <h1>React Count Timer Example</h1>
+      <h1>React Count Timer Example</h1>
       <CountTimer countType={'down'} initHours={2}>
         {({ days, hours, minutes, seconds }) => {
           return (
